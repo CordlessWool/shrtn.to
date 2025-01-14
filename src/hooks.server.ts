f2a194db812c1a49cd69ff81a21cacf307381bbd
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-01-14 14:18:05 +0100

    set interval to cleanup old entries

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,17 @@
 import { sequence } from '@sveltejs/kit/hooks';
 import type { Handle } from '@sveltejs/kit';
 import * as auth from '$lib/server/auth.js';
+import { db, schema } from '$lib/server/db/index.js';
+import { lte } from 'drizzle-orm';
+
+const HOUR = 3600000;
+const TEN_MINUTES = 600000;
+const cleanupLinks = () => {
+	// Remove expired links
+	db.delete(schema.link).where(lte(schema.link.expiresAt, new Date())).run();
+};
+
+setInterval(cleanupLinks, TEN_MINUTES);
 
 const handleAuth: Handle = async ({ event, resolve }) => {
 	const sessionToken = event.cookies.get(auth.sessionCookieName);
