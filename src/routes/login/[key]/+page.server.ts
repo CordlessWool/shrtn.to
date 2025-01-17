4377bad7dcfcb7e4a84aa3940ddad5e065c82af2
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-01-17 18:24:53 +0100

    add links to an existing user on login

--- src/routes/login/[key]/+page.server.ts
+++ src/routes/login/[key]/+page.server.ts
@@ -6,6 +6,8 @@ import { error, redirect } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async (event) => {
 	const { key } = event.params;
+	const { locals } = event;
+
 	const user = db
 		.select({
 			id: schema.user.id,
@@ -20,8 +22,19 @@ export const load: PageServerLoad = async (event) => {
 		error(401, 'Not Authorized');
 	}
 
+	if (locals.user?.temp === false && locals.user?.id !== user.id) {
+		//TODO: redirect to a page to switch user
+		error(401, 'Logout to switch user');
+	}
+
 	if (user.temp === true) {
 		db.update(schema.user).set({ temp: false }).where(eq(schema.user.id, user.id)).run();
+	} else if (locals.user?.id && locals.user.temp === true) {
+		db.update(schema.link)
+			.set({ userId: user.id })
+			.where(eq(schema.link.userId, locals.user.id))
+			.run();
+		db.delete(schema.user).where(eq(schema.user.id, locals.user.id)).run();
 	}
 	db.delete(schema.magicLink).where(eq(schema.magicLink.id, key)).run();
 
