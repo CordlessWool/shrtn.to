2751fce0a57e69931f0a686eec24faa89fb994b7
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-01-27 21:04:08 +0100

    give button transparent and error mode

--- src/lib/comp/Button.svelte
+++ src/lib/comp/Button.svelte
@@ -1,21 +1,69 @@
 <script lang="ts">
-	const { children, ...props } = $props();
+	const { children, transparent = false, error = false, disabled = false, ...props } = $props();
 </script>
 
-<button {...props}>
+<button disabled={error === true || disabled === true} class:error class:transparent {...props}>
 	{@render children()}
 </button>
 
 <style lang="postcss">
 	button {
-		@apply flex flex-row items-center justify-center gap-3 text-nowrap;
-		@apply bg-teal-400 p-3 hover:bg-teal-500 hover:opacity-100;
-		@apply transform rounded-lg shadow-md transition-all duration-300 ease-in-out hover:scale-105 focus:outline-none focus:ring-2 focus:ring-teal-600;
+		@apply flex flex-row items-center justify-center gap-2 text-nowrap;
+		@apply p-3;
+		@apply rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-600;
+	}
+
+	button:enabled {
+		@apply transform transition-all duration-300 ease-in-out hover:scale-105;
+	}
+
+	button:not(.transparent) {
+		@apply bg-teal-400 shadow-md;
+
+		&.error {
+			@apply bg-red-400;
+		}
+
+		&:enabled {
+			@apply hover:bg-teal-500;
+		}
+	}
+
+	button.transparent {
+		@apply bg-transparent text-teal-700;
+
+		&.error {
+			@apply text-red-700;
+		}
+
+		&:enabled {
+			@apply hover:text-teal-800;
+		}
 	}
 
 	:global(.dark) {
-		button {
+		button:not(.transparent) {
 			@apply bg-teal-800 hover:bg-teal-700;
+
+			&.error {
+				@apply hover:bg-red-700;
+			}
+
+			&:enabled {
+				@apply hover:bg-teal-700;
+			}
+		}
+
+		button.transparent {
+			@apply text-teal-600;
+
+			&.error {
+				@apply text-red-400;
+			}
+
+			&:enabled {
+				@apply hover:text-teal-500;
+			}
 		}
 	}
 </style>
