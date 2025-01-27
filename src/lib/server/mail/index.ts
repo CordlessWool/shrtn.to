9a26b015cf07fa760aadde3992edd74accd95cde
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-01-27 02:01:26 +0100

    simplify mail styling

--- src/lib/server/mail/index.ts
+++ src/lib/server/mail/index.ts
@@ -19,18 +19,6 @@ const transporter = nodemailer.createTransport(
 	encodeURI(`smtps://${connection.auth.user}:${connection.auth.pass}@${connection.host}`)
 );
 
-function hexToRgb(hex: `#${string}`) {
-	const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-	if (!result) {
-		throw new Error('Invalid hex color');
-	}
-	return {
-		r: parseInt(result[1], 16),
-		g: parseInt(result[2], 16),
-		b: parseInt(result[3], 16)
-	};
-}
-
 export const mail = async (to: string, subject: string, text: string, html?: string) => {
 	await transporter.sendMail({
 		from: env.MAIL_FROM,
@@ -45,7 +33,7 @@ export const sendVerificationMail = async (to: string, key: string, theme: THEME
 	const html = mustache.render(VERIFICATION_TEMPLATE, {
 		bg: theme === THEME.DARK ? '#27272a' : '#f4f4f5',
 		color: theme === THEME.DARK ? '#e4e4e7' : '#27272a',
-		accent: theme === THEME.DARK ? hexToRgb('#115e59') : hexToRgb('#2dd4bf'),
+		accent: theme === THEME.DARK ? '#115e59' : '#2dd4bf',
 		key
 	});
 	const htmlWithInlineStyles = juice(html);
