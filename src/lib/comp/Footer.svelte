4ae51a7be6161d1307cda10fee970f29fbf84f68
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-01-28 13:25:43 +0100

    saperate footer

---
+++ src/lib/comp/Footer.svelte
@@ -0,0 +1,7 @@
+<script>
+	import * as m from '$lib/paraglide/messages';
+</script>
+
+<footer>
+	{m.made_with_love()} <a href="https://dropanote.de">CordlessWool</a>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,14 @@
 <script lang="ts">
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import { i18n } from '$lib/i18n';
-	import * as m from '$lib/paraglide/messages';
 
 	import '../app.css';
+	import Footer from '$lib/comp/Footer.svelte';
 	let { children } = $props();
 </script>
 
 <ParaglideJS {i18n}>
 	{@render children()}
 
-	<footer>
-		{m.made_with_love()} <a href="https://dropanote.de">CordlessWool</a>
-	</footer>
+	<Footer />
 </ParaglideJS>
