69939a58d73698dbed6605b3f80e805da7b4f231
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2025-01-28 13:54:55 +0100

    add i18n to error messages

--- src/lib/helper/form.ts
+++ src/lib/helper/form.ts
@@ -13,6 +13,7 @@ import {
 import { nanoid } from 'nanoid';
 
 import * as v from 'valibot';
+import * as m from '$lib/paraglide/messages';
 
 export const getString = (
 	value: string | FormDataEntryValue | null,
@@ -52,8 +53,16 @@ const getTTLUser = (): [number, string][] => ttlMapFromStep(MAX_TTL_USER);
 
 export const getTTLs = (loggedin: boolean) => (loggedin ? getTTLUser() : getTTLTempUser());
 
+const LinkValueSchema = v.pipe(
+	v.string(),
+	v.trim(),
+	v.minLength(2, m.error_link_min_lenght),
+	v.transform((url) => (url.match(/^[A-Za-z]+:\/\//) ? url : `https://${url}`)),
+	v.url(m.error_invalid_url)
+);
+
 export const LinkSchemaSignedUp = v.object({
-	link: v.pipe(v.string(), v.minLength(2)),
+	link: LinkValueSchema,
 	ttl: v.pipe(v.optional(v.number(), HOUR_IN_MS), v.maxValue(ttlFromStep(MAX_TTL_USER))),
 	short: v.pipe(
 		v.optional(v.string(), () => nanoid(SHORTEN_LENGTH)),
@@ -62,13 +71,7 @@ export const LinkSchemaSignedUp = v.object({
 });
 
 export const LinkSchemaTemp = v.object({
-	link: v.pipe(
-		v.string(),
-		v.minLength(2),
-		v.trim(),
-		v.transform((url) => (url.match(/^[A-Za-z]+:\/\//) ? url : `https://${url}`)),
-		v.url()
-	),
+	link: LinkValueSchema,
 	ttl: v.pipe(v.optional(v.number(), HOUR_IN_MS), v.maxValue(ttlFromStep(MAX_TTL_TEMP))),
 	short: v.pipe(
 		v.fallback(v.pipe(v.string(), v.trim(), v.minLength(1)), () => nanoid(SHORTEN_LENGTH))
@@ -79,12 +82,12 @@ export const getLinkSchema = (loggedin: boolean) =>
 	loggedin ? LinkSchemaSignedUp : LinkSchemaTemp;
 
 export const LoginMailSchema = v.object({
-	email: v.pipe(v.string(), v.trim(), v.email()),
+	email: v.pipe(v.string(), v.trim(), v.email(m.error_invalid_email)),
 	theme: v.optional(v.pipe(v.number(), v.enum(THEME)))
 });
 
 export const VerificationSchema = v.object({
-	key: v.pipe(v.string(), v.trim(), v.minLength(1, 'Please enter key'), v.toLowerCase())
+	key: v.pipe(v.string(), v.trim(), v.minLength(1, m.error_empty_key), v.toLowerCase())
 });
 
 export const ThemeSchema = v.object({
